3c74f6a5130078442a84f5438d299cd5e5e96ad0
Adam Rogers <20117311@setu.ie>
2026-03-12 14:31:25 +0000

    Completed Add Functions Lab

--- routes.js
+++ routes.js
@@ -15,6 +15,9 @@ router.get('/dashboard', dashboard.createView);
 router.get('/about', about.createView);
 router.get('/error', (request, response) => response.status(404).end('Page not found.'));
 router.get('/playlist/:id', playlist.createView);
+router.post('/playlist/:id/addsong', playlist.addSong);
+router.post('/dashboard/addplaylist', dashboard.addPlaylist);
+
 
 
 export default router;
