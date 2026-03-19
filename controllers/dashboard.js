1414ae18ea1f2555466bf36c4c47b85ce9f3e416
Adam Rogers <20117311@setu.ie>
2026-03-19 14:48:44 +0000

    Completed Delete Functions Lab

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -19,15 +19,21 @@ const dashboard = {
     response.render('dashboard', viewData);
   },
 
-  addPlaylist(request, response) {
-    const newPlayList = {
+addPlaylist(request, response) {
+    const timestamp = new Date();
+
+    const newPlaylist = {
       id: uuidv4(),
       title: request.body.title,
-      songs: [],
+      date: timestamp,
+      rating: parseInt(request.body.rating),
+      songs: []
     };
-    playlistStore.addPlaylist(newPlayList);
+
+    playlistStore.addPlaylist(newPlaylist);
     response.redirect('/dashboard');
 },
+
   deletePlaylist(request, response) {
     const playlistId = request.params.id;
     logger.debug(`Deleting Playlist ${playlistId}`);
