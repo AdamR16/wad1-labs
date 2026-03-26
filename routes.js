afaf77430f9b57926998471b2cfce82349ddc02e
Adam Rogers <20117311@setu.ie>
2026-03-26 12:59:28 +0000

    Search and sort functions added

--- routes.js
+++ routes.js
@@ -8,6 +8,8 @@ import start from './controllers/start.js';
 import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js';
 import playlist from './controllers/playlist.js';
+import stats from './controllers/stats.js';
+
 
 
 router.get('/', start.createView);
@@ -20,6 +22,11 @@ router.post('/dashboard/addplaylist', dashboard.addPlaylist);
 router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
 router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
 router.post('/playlist/:id/updatesong/:songid', playlist.updateSong);
+router.get('/stats', stats.createView);
+router.get('/searchCategory', dashboard.createView);
+router.get('/sortData', dashboard.createView);
+
+
 
 
 
